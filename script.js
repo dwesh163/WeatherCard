a0991af05fd527cd8ec2f81ad76970f5e9794819
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-12-02 22:31:41 +0100

    [add] style

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ const options = {
 async function fetchAPI(api_key, city) {
     $(".loadingBox").show()
     $(".unavailableData").hide()
+    hideInfo()
 
     const url  = `http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city}`;
     const response = await fetch(url)
     if (!response.ok) {
         $(".loading").hide()
         $(".unavailableData").show()
+        hideInfo()
         return
     }
     const data = await response.json();
@@ -22,13 +24,27 @@ async function fetchAPI(api_key, city) {
     $(".locationText").text(data["location"]["name"] + ", " + data["location"]["country"])
 
     $("section").removeClass("error");
+    $("section").addClass("sunny");
     await $(".loadingBox").hide()
+    await showInfo()
 }
 
 const api_key = localStorage.getItem("WeatherCard")
 const city = "Chabrey";
 
 $(".loadingBox").hide()
+
+function hideInfo() {
+    $(".sunnySun").hide()
+    $(".iconSun").hide()
+    $(".textBox").hide()
+}
+
+function showInfo() {
+    $(".sunnySun").show()
+    $(".iconSun").show()
+    $(".textBox").show()
+}
 
 
 
