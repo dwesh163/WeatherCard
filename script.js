2fda058b130d80709ca4a9c25958fa2c1f0de932
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-12-03 19:45:34 +0100

    [add] more background

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const options = {
     weekday: 'long',
     month: 'long',
     day: 'numeric',
-  };
+};
 
-async function fetchAPI(api_key, city) {
-    $(".weatherLoadingBox").show()
-    $(".weatherUnavailableData").hide()
-    hideInfo()
-
-    const url  = `http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city}`;
+async function fetchAPI(api_key, city, isReload) {
+    if(isReload){
+        $(".weatherLoadingBox").show()
+        $(".weatherUnavailableData").hide()
+        hideInfo()
+    }
+    
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city}`;
     const response = await fetch(url)
     if (!response.ok) {
         $(".weatherLoading").hide()
@@ -19,33 +21,48 @@ async function fetchAPI(api_key, city) {
     }
     const data = await response.json();
 
+    let isNight = ""
+
     $(".weatherTemperatureText").text(data["current"]["temp_c"])
     $(".weatherTimeText").text(new Date().toLocaleDateString("en-us", options))
     $(".weatherLocationText").text(data["location"]["name"] + ", " + data["location"]["country"])
 
+    if(data["current"]["is_day"] == 0){
+        isNight = "_night"
+    }
+
+    console.log(`weather_${data["current"]["condition"]["text"].replaceAll(" ", "_")}${isNight}`);
+    
     $(".weatherSection").removeClass("weatherError");
-    $(".weatherSection").addClass("weatherSunny");
+    $(".weatherSection").addClass(`weather_${data["current"]["condition"]["text"].replaceAll(" ", "_")}${isNight}`);
     await $(".weatherLoadingBox").hide()
     await showInfo()
 }
 
 const api_key = localStorage.getItem("WeatherCard")
-const city = "Chabrey";
+const city = "paris";
 
 $(".weatherLoadingBox").hide()
 
 function hideInfo() {
-    $(".weatherSunnySun").hide()
     $(".weatherIconSun").hide()
     $(".weatherTextBox").hide()
 }
 
 function showInfo() {
-    $(".weatherSunnySun").show()
     $(".weatherIconSun").show()
     $(".weatherTextBox").show()
 }
 
+setInterval(() => {
+    fetchAPI(api_key, city, false)
+
+}, 30000);
+
+fetchAPI(api_key, city, true)
+
+//weather_Light_rain
+//weather_Moderate_rain_night
 
 
-fetchAPI(api_key, city)
+
